0a076a425d36a677102af494ba112bc93f5786e5
AlejandroCarames <acrskater@gmail.com>
2025-04-10 21:04:19 +0200

    implement react&vite #73

---
+++ staff/alejandro-carames/product/app/view/Home.jsx
@@ -0,0 +1,11 @@
+export const Home =  () => {
+    console.log('home -> render')
+
+    return <div className="p-20px">
+        <i>Logo</i>
+
+        <h1>Hello, world!</h1>
+
+        <button className="black-button">Logout</button>
+    </div>
+}
